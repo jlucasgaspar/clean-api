e702571981208c447204a2fc65b6d289bae62cb9
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-02-20 21:46:20 -0300

    refactor: make httpHelper methods more readable

--- src/presentation/helpers/httpHelper.ts
+++ src/presentation/helpers/httpHelper.ts
@@ -1,6 +1,17 @@
 import { HttpResponse } from '../protocols'
 import { ServerError } from '../errors'
 
-export const badRequest = (err: Error): HttpResponse => ({ statusCode: 400, body: err })
-export const serverError = (): HttpResponse => ({ statusCode: 500, body: new ServerError() })
-export const ok = (data: any): HttpResponse => ({ statusCode: 200, body: data })
+export const badRequest = (err: Error): HttpResponse => ({
+    statusCode: 400,
+    body: err
+})
+
+export const serverError = (): HttpResponse => ({
+    statusCode: 500,
+    body: new ServerError()
+})
+
+export const ok = (data: any): HttpResponse => ({
+    statusCode: 200,
+    body: data
+})
