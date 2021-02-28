aba2ec92b5454f6491318400881a4b0885e73d9f
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-02-28 20:03:03 -0300

    test: ensure LogController returns the same response of its intern controller

--- src/main/decorator/Log.spec.ts
+++ src/main/decorator/Log.spec.ts
@@ -50,4 +50,24 @@ describe('LogController Decorator', () => {
 
         expect(handleSpy).toHaveBeenCalledWith(httpRequest)
     })
+
+    test('Should return the same result of the intern controller', async () => {
+        const { sut } = makeSut()
+
+        const httpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_passwordConfirmation'
+            }
+        }
+
+        const httpResponse = await sut.handle(httpRequest)
+
+        expect(httpResponse).toEqual({
+            statusCode: 200,
+            body: httpRequest
+        })
+    })
 })
