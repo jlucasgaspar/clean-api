c78fe77ac2010b9334ac2f06895bd224c9c4d051
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-04 11:11:49 -0300

    feat: ensure DbAuthentication UseCase call HashComparer with correct id

--- src/data/useCases/Authentication/DbAuthentication.ts
+++ src/data/useCases/Authentication/DbAuthentication.ts
@@ -1,17 +1,21 @@
 import { Authentication, AuthModel } from '../../../domain/useCases/Authentication'
 import { HashComparer } from '../../protocols/cryptography/HashComparer'
+import { TokenGenerator } from '../../protocols/cryptography/TokenGenerator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/LoadAccountByEmailRepository'
 
 export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
     private readonly hashComparer: HashComparer
+    private readonly tokenGenerator: TokenGenerator
 
     constructor(
         loadAccountByEmailRepository: LoadAccountByEmailRepository,
-        hashComparer: HashComparer
+        hashComparer: HashComparer,
+        tokenGenerator: TokenGenerator
     ) {
         this.loadAccountByEmailRepository = loadAccountByEmailRepository
         this.hashComparer = hashComparer
+        this.tokenGenerator = tokenGenerator
     }
 
     async auth(authData: AuthModel): Promise<string> {
@@ -26,5 +30,7 @@ export class DbAuthentication implements Authentication {
         if (!passwordIsValid) {
             return null
         }
+
+        const token = await this.tokenGenerator.generate(account.id)
     }
 }
