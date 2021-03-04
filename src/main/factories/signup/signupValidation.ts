53f59ce5ed2f98c1b2e8a3cfbab898892c0dcecb
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-04 08:38:14 -0300

    refactor: finish correct import paths

--- src/main/factories/signup/signupValidation.ts
+++ src/main/factories/signup/signupValidation.ts
@@ -1,9 +1,9 @@
-import { CompareFieldsValidation } from '../../presentation/helpers/validators/compareFieldsValidation'
-import { EmailValidation } from '../../presentation/helpers/validators/emailValidation'
-import { RequiredFieldValidation } from '../../presentation/helpers/validators/requiredFieldValidation'
-import { Validation } from '../../presentation/helpers/validators/Validation'
-import { ValidationComposite } from '../../presentation/helpers/validators/validationComposite'
-import { EmailValidatorAdapter } from '../../utils/EmailValidatorAdapter'
+import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compareFieldsValidation'
+import { EmailValidation } from '../../../presentation/helpers/validators/emailValidation'
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/requiredFieldValidation'
+import { Validation } from '../../../presentation/helpers/validators/Validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validationComposite'
+import { EmailValidatorAdapter } from '../../../utils/EmailValidatorAdapter'
 
 export const makeSignUpValidation = (): ValidationComposite => {
     const validations: Validation[] = []
