7b993175bde88540eb9f2f83ad5e76be27317819
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-10 10:58:40 -0300

    test: ensure Survey Mongo Repository return null on add survey success

---
+++ src/infra/db/mongodb/surveyRepository/SurveyMongoRepository.spec.ts
@@ -0,0 +1,37 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongoHelper'
+import { SurveyMongoRepository } from './SurveyMongoRepository'
+
+const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+}
+
+let surveyCollection: Collection
+
+describe('Survey Mongo Repository', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        surveyCollection = await MongoHelper.getCollection('survey')
+        await surveyCollection.deleteMany({})
+    })
+
+    test('Should return null on add survey success', async () => {
+        const sut = makeSut()
+
+        await sut.add({
+            question: 'any_question',
+            answers: [{ answer: 'any_answer', image: 'any_image' }, { answer: 'any_answer_2' }]
+        })
+
+        const survey = await surveyCollection.findOne({ question: 'any_question' })
+
+        expect(survey).toBeTruthy()
+    })
+})
