7d8df94e49adda7db87a18159a8927bd08f01146
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-10 10:58:26 -0300

    feat: ensure Survey Mongo Repository return null on add survey success

---
+++ src/infra/db/mongodb/surveyRepository/SurveyMongoRepository.ts
@@ -0,0 +1,11 @@
+import { AddSurveyModel } from '../../../../domain/useCases/AddSurvey';
+import { AddSurveyRepository } from '../../../../data/protocols/db/survey/AddSurveyRepository';
+import { MongoHelper } from '../helpers/mongoHelper';
+
+export class SurveyMongoRepository implements AddSurveyRepository {
+    async add(surveyData: AddSurveyModel): Promise<void> {
+        const surveyCollection = await MongoHelper.getCollection('survey')
+
+        await surveyCollection.insertOne(surveyData)
+    }
+}
