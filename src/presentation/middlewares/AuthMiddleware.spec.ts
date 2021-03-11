f810a42bbca59c48ec59f3751d8b43686b224341
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-11 10:55:39 -0300

    test: ensure Auth Middleware call LoadAccountByToken with corect accessToken

--- src/presentation/middlewares/AuthMiddleware.spec.ts
+++ src/presentation/middlewares/AuthMiddleware.spec.ts
@@ -1,12 +1,37 @@
+import { LoadAccountByToken } from './../../domain/useCases/LoadAccountByToken';
 import { AuthMiddleware } from './AuthMiddleware';
 import { forbidden } from './../helpers/http/httpHelper';
 import { AccessDenied } from '../errors';
+import { HttpRequest } from '../protocols';
+import { AccountModel } from '../../domain/models/Account';
+
+const makeFakeAccount = (): AccountModel => ({
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email@mail.com',
+    password: 'valid_password'
+})
+
+class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load(accessToken: string, role?: string): Promise<AccountModel> {
+        return Promise.resolve(makeFakeAccount())
+    }
+}
 
 describe('Auth Middleware', () => {
     test('should return 403 if no x-access-token exists in headers', async () => {
-        //const httpRequest: HttpRequest = { headers: {}}
-        const sut = new AuthMiddleware();
-        const httpResponse = await sut.handle({});
+        const loadAccountByTokenStub = new LoadAccountByTokenStub();
+        const sut = new AuthMiddleware(loadAccountByTokenStub);
+        const httpResponse = await sut.handle({}); //const httpRequest: HttpRequest = { headers: {}}
         expect(httpResponse).toEqual(forbidden(new AccessDenied()));
+    });
+
+    test('should call LoadAccountByToken with corect accessToken', async () => {
+        const loadAccountByTokenStub = new LoadAccountByTokenStub();
+        const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+        const httpRequest: HttpRequest = { headers: { 'x-access-token': 'any_token' }}
+        const sut = new AuthMiddleware(loadAccountByTokenStub);
+        await sut.handle(httpRequest);
+        expect(loadSpy).toHaveBeenCalledWith('any_token');
     });
 });
